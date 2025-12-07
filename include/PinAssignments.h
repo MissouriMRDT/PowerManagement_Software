8d90329dd2d7ed94b80d713c5a769d136815c43a
aidengithub12 <147771123+aidengithub12@users.noreply.github.com>
2025-12-07 15:03:59 -0600

    PinAssignments

---
+++ include/PinAssignments.h
@@ -0,0 +1,39 @@
+//enable pins
+#define LC_ENABLE 5
+#define M2_ENABLE 28
+#define Motor_ENABLE 4
+#define M9_ENABLE 11
+#define NS_ENABLE 12
+#define AUX_ENABLE 6
+
+//Cell Sense Pins
+#define CELL_SENSE_1_PIN 41
+#define CELL_SENSE_2_PIN 14
+#define CELL_SENSE_3_PIN 15
+#define CELL_SENSE_4_PIN 16
+#define CELL_SENSE_5_PIN 17
+#define CELL_SENSE_6_PIN 18
+
+//current sense pins
+#define PACK_CURRENT_SENSE_PIN 20
+#define AUX_CURRENT_SENSE_PIN 40
+#define NS_CURRENT_SENSE_PIN 24
+#define M2_CURRENT_SENSE_PIN 27
+#define M9_CURRENT_SENSE_PIN 26
+#define LC_CURRENT_SENSE_PIN 19
+
+//LCD pins
+#define LCD_RS 21
+#define LCD_RW 22
+#define LCD_EN 3
+#define LCD_D0 2
+#define LCD_D1 1
+#define LCD_D2 39
+#define LCD_D3 38
+#define LCD_D4 37
+#define LCD_D5 36
+#define LCD_D6 35
+#define LCD_D7 34
+
+//Buzzer Pin
+#define BUZZER_PIN 23
